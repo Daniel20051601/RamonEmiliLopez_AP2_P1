f20940b0d5c5419bc63858c5f63bc50b79fbdb01
Danielvi <danielvilopez@gmail.com>
2025-10-05 23:29:33 -0400

    Agrega `NavHost` y la anotación @AndroidEntryPoint al MainActivity

--- app/src/main/java/edu/ucne/ramonemililopez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/ramonemililopez_ap2_p1/MainActivity.kt
@@ -6,42 +6,39 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.ramonemililopez_ap2_p1.tareas.local.navigation.AppNavHost
 import edu.ucne.ramonemililopez_ap2_p1.ui.theme.RamonEmiliLopez_AP2_P1Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RamonEmiliLopez_AP2_P1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    Scaffold(
+                    ) { innerPadding ->
+                        AppNavHost(
+                            navController = navController,
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RamonEmiliLopez_AP2_P1Theme {
-        Greeting("Android")
-    }
-}
+
