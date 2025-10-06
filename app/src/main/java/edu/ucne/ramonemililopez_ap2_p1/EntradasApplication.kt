ee0f6fad877cc412b51d2069725368181eccbe79
Danielvi <danielvilopez@gmail.com>
2025-10-05 23:19:27 -0400

    Crea la clase EntradasApplication anotada con `@HiltAndroidApp`

---
+++ app/src/main/java/edu/ucne/ramonemililopez_ap2_p1/EntradasApplication.kt
@@ -0,0 +1,8 @@
+package edu.ucne.ramonemililopez_ap2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EntradasApplication: Application() {
+}
